4f66c00dd971277ea8d9c75c38dc9dd52da6e805
EmotionlessDev <emotionlesscode@gmail.com>
2024-09-28 23:48:58 +1000

    added theme & mypy.ini

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from ui.MainWindow import MainWindow
+import qdarktheme
 
 
 def main():
     app = QApplication(sys.argv)
+    qdarktheme.setup_theme()
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
